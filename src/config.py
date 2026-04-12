380d980893272ab3da9b835df263c2795bb7c118
Vitman <vitman_958@hotmail.com>
2026-04-12 16:57:08 +0300

    Добавлены новые пути

--- src/config.py
+++ src/config.py
@@ -2,4 +2,8 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
-DATA_RAW_PATH = BASE_DIR / "data" / "spam.csv"
+DATA_RAW_PATH = BASE_DIR / "data"/ "raw" / "spam.csv"
+DATA_PROCESSED_PATH = BASE_DIR / "data" / "processed"
+MODELS_DIR = BASE_DIR / "models"
+LOGS_DIR = BASE_DIR / "logs"
+NLTK_DATA_PATH = BASE_DIR / "nltk_data" 
