ef6344db0155fec681c17d7ab1b7aeb6a9ebb8f6
Vitman <vitman_958@hotmail.com>
2026-04-19 14:49:16 +0300

    Реализован модуль обработки текста

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -12,29 +12,39 @@ nltk.data.path.append(str(NLTK_DATA_PATH))
 
 
 class TextPreprocessor:
-    def __init__(self, stop_words: List[str] = None):
-        pass
+    def __init__(self, custom_stop_words: List[str] = None):
+        self.lemmatizer = WordNetLemmatizer()
+        base_stop_words = set(stopwords.words('english'))
+        
+        if custom_stop_words:
+            base_stop_words.update(custom_stop_words)
+
+        self.stop_words = base_stop_words
 
     def lowercase(self, text: str) -> str:
-        pass
+        return text.lower()
     
     def remove_punctuation(self, text: str) -> str:
-        # Подсказка: str.translate() + str.maketrans()
-        pass
+        trans_table = str.maketrans('', '', string.punctuation)
+        return text.translate(trans_table)
     
     def tokenize(self, text: str) -> List[str]:
-        # Вернёт список слов
-        pass
+        return nltk.word_tokenize(text)
     
     def remove_stopwords(self, tokens: List[str]) -> List[str]:
-        # Принимает список, возвращает список
-        pass
+        return [token for token in tokens if token not in self.stop_words]
     
     def lemmatize(self, tokens: List[str]) -> List[str]:
-        # Принимает список, возвращает список
-        pass
+        return [self.lemmatizer.lemmatize(token) for token in tokens]
     
     def process(self, text: str) -> str:
-        """Основной метод: цепочка всех преобразований"""
-        # Реализуй пайплайн
-        pass
+        text = self.lowercase(text)
+        text = text.replace('-', ' ')
+        text = self.remove_punctuation(text)
+        tokens = self.tokenize(text)
+        tokens = self.remove_stopwords(tokens)
+        tokens = self.lemmatize(tokens)
+        return " ".join(tokens)
+
+
+proc = TextPreprocessor()
