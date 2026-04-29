270dac81496865178deaaefc861bfb848fa7ad4e
Vitman <vitman_958@hotmail.com>
2026-04-29 16:32:50 +0300

    Переделана работа с путями

--- src/config.py
+++ src/config.py
@@ -1,9 +1,27 @@
+import sys
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent.parent
-DATA_RAW_PATH = BASE_DIR / "data"/ "raw" / "spam.csv"
-DATA_PROCESSED_PATH = BASE_DIR / "data" / "processed"
-MODELS_DIR = BASE_DIR / "models"
-LOGS_DIR = BASE_DIR / "logs"
-NLTK_DATA_PATH = BASE_DIR / "nltk_data" 
+import nltk
+
+
+def get_resource_path(relative_path: str) -> Path:
+    """Возвращает абсолютный путь к файлу"""
+
+    if getattr(sys, 'frozen', False):
+        
+        base_path = Path(sys._MEIPASS)
+    else:
+        
+        base_path = Path(__file__).resolve().parent.parent
+    return base_path / relative_path
+
+
+BASE_DIR = get_resource_path("")
+DATA_RAW_PATH = get_resource_path("data/raw")
+DATA_PROCESSED_PATH = get_resource_path("data/processed")
+MODELS_DIR = get_resource_path("models")
+NLTK_DATA_PATH = get_resource_path("nltk_data")
+
+if str(NLTK_DATA_PATH) not in nltk.data.path:
+    nltk.data.path.append(str(NLTK_DATA_PATH))
